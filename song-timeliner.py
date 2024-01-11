03351eb577662e7e8d834544fc8e8f44cb691ba9
Alex Leeper <alixvox@gmail.com>
2024-01-11 15:35:51 -0500

    album differentiation is gone (doesn't work on sc)

--- song-timeliner.py
+++ song-timeliner.py
@@ -1,7 +1,6 @@
 import argparse
 import youtube_dl
 from mutagen import File
-from mutagen.id3 import ID3NoHeaderError
 import os
 import csv
 
@@ -20,88 +19,104 @@ def extract_metadata(file_path):
     title = audio.get('title', [os.path.basename(file_path).split('.')[0]])[0]
     artist = audio.get('artist', ['Unknown'])[0]
     date = audio.get('date', [''])[0]
-    album = audio.get('album', ['Unknown'])[0]
 
-    return artist, title, date, album
-
-def organize_songs_into_albums(artist_data):
-    for artist, title, _, album in artist_data:
-        source_path = os.path.join(artist, f'{title}.mp3')
-        album = album if album and album != 'Unknown' else title
-        album_path = os.path.join(artist, album)
-
-        if not os.path.exists(album_path):
-            os.makedirs(album_path)
-
-        if os.path.exists(source_path):
-            os.rename(source_path, os.path.join(album_path, f'{title}.mp3'))
+    return artist, title, date
 
 def create_csv(artist_data, filename):
     with open(filename, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['Artist', 'Title', 'Release Date', 'Album'])
+        writer.writerow(['Artist', 'Title', 'Release Date'])
         for data in artist_data:
             writer.writerow(data)
 
+def convert_args_to_urls(arguments):
+    artist_urls = {}
+    bc_albums = {}
+    processing_bc = False
+    current_bc_artist = None
+
+    for arg in arguments:
+        if arg == 'bc':
+            processing_bc = True
+        elif arg == 'cb':
+            processing_bc = False
+            current_bc_artist = None
+        elif processing_bc:
+            if current_bc_artist is None:
+                current_bc_artist = arg
+            else:
+                bc_albums.setdefault(current_bc_artist, []).append(f'https://{current_bc_artist}.bandcamp.com/album/{arg}')
+        else:
+            artist_id = arg
+            if artist_id.startswith('UC'):
+                artist_urls[artist_id] = f'https://www.youtube.com/channel/{artist_id}'
+            else:
+                artist_urls[artist_id] = f'https://soundcloud.com/{artist_id}'
+
+    return artist_urls, bc_albums
+
 def main():
     parser = argparse.ArgumentParser(description='Download and timeline songs.')
-    parser.add_argument('artists', nargs='*', help='List of artist IDs (optional)')
+    parser.add_argument('args', nargs=argparse.REMAINDER, help='List of artist IDs and Bandcamp albums')
     parser.add_argument('-d', '--download', action='store_true', help='Download new content for artists')
-    parser.add_argument('-f', '--folder', action='store_true', help='Organize songs into album folders')
     args = parser.parse_args()
 
-    # If no artist IDs are provided, process all existing directories
-    if not args.artists:
-        args.artists = [d for d in os.listdir('.') if os.path.isdir(d)]
+    # Convert arguments to URLs
+    artist_urls, bc_album_urls = convert_args_to_urls(args.args)
 
     ydl_opts = {
         'format': 'bestaudio/best',
-        'postprocessors': [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            },
-            {
-                'key': 'FFmpegMetadata',
-            },
-        ],
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }, {
+            'key': 'FFmpegMetadata',
+        }],
         'addmetadata': True
     }
 
     artist_data = []
 
-    for artist_id in args.artists:
-        artist_dir = os.path.join(os.getcwd(), artist_id)
-
-        # Determine if the artist is from YouTube or SoundCloud
-        if artist_id.startswith('UC'):
-            artist_url = f'https://www.youtube.com/channel/{artist_id}'
-        else:
-            artist_url = f'https://soundcloud.com/{artist_id}'
-
-        # Download only if -d flag is used
-        if args.download:
+    # Download and process artists
+    if args.download:
+        for artist_id, url in artist_urls.items():
+            artist_dir = os.path.join(os.getcwd(), artist_id)
             if not os.path.exists(artist_dir):
                 os.makedirs(artist_dir)
-            download_audio(artist_url, artist_id, ydl_opts)
+            download_audio(url, artist_id, ydl_opts)
 
-        # Process each file in the artist's directory
-        for file in os.listdir(artist_dir):
-            if file.endswith('.mp3'):
-                file_path = os.path.join(artist_dir, file)
-                metadata = extract_metadata(file_path)
-                if metadata:
-                    artist_data.append([artist_id, metadata[1], metadata[2], metadata[3]])
-                else:
-                    artist_data.append([artist_id, os.path.splitext(file)[0], '', ''])
+        # Process Bandcamp albums
+        for artist_id, albums in bc_album_urls.items():
+            artist_dir = os.path.join(os.getcwd(), 'bc-' + artist_id)
+            if not os.path.exists(artist_dir):
+                os.makedirs(artist_dir)
+            for album_url in albums:
+                download_audio(album_url, 'bc-' + artist_id, ydl_opts)
 
-    # Organize songs into folders based on album name
-    if args.folder:
-        organize_songs_into_albums(artist_data)
+    # Process each file in the artist's directory
+    for artist_id in os.listdir('.'):
+        if os.path.isdir(artist_id):
+            artist_dir = os.path.join(os.getcwd(), artist_id)
+            for file in os.listdir(artist_dir):
+                if file.endswith('.mp3'):
+                    file_path = os.path.join(artist_dir, file)
+                    metadata = extract_metadata(file_path)
+                    if metadata:
+                        artist_data.append([artist_id, metadata[1], metadata[2]])
+                    else:
+                        artist_data.append([artist_id, os.path.splitext(file)[0], ''])
+
+    # Get a list of artist directories
+    artist_dirs = [d for d in os.listdir('.') if os.path.isdir(d) and not d.startswith('.')]
+    
+    # Check if there are any artist directories
+    if not artist_dirs:
+        print("Error: No artist folders found in the directory.")
+        return
 
     # Create CSV file
-    csv_filename = f"stl-{'-'.join(args.artists)}.csv"
+    csv_filename = f"stl-{'-'.join(artist_dirs)}.csv"
     create_csv(artist_data, csv_filename)
 
 if __name__ == "__main__":
